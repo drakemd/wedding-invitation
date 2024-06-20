825f3c27505c5f45da07333a711f2c589373388e
Indraga Martiyana <drakemd@Indragas-MacBook-Air.local>
2024-06-20 22:04:07 +0700

    feat: change header title

--- app/wedding/components/header.tsx
+++ app/wedding/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header () {
     return (
         <header className="flex items-center justify-between p-4">
-        <h1 className="text-2xl font-bold">Wedding</h1>
+        <h1 className="text-2xl font-bold">My Wedding Invitation</h1>
         </header>
     );
 }
